345025438fc96903721ae767010dfdad6284ea9a
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2022-01-20 18:17:48 +0900

    Update 백준 2765.py

--- 백준 2765.py
+++ 백준 2765.py
@@ -1,2 +1,10 @@
-size, rpm, minute = map(int, input().split())
-
+i = 0
+while True:    
+    size, rpm, second = map(float, input().split())
+    if rpm == 0 or second == 0:
+        break
+    pi = 3.1415927
+    distance = ((pi*size*rpm)/12)/5280
+    mph = (distance / second)*3600
+    i += 1
+    print('Trip #{}: {} {}'.format(i, format(distance, ".2f"), format(mph, ".2f")))
